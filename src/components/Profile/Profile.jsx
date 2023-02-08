d381a0c3c3e3cec80da5202a98f34f7d41414b2d
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-02-08 22:39:16 +0200

    0802233

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,10 @@
+import css from './Profile.module.css'
+import PropTypes from 'prop-types';
+export const Profile = () => {
+
+  return <div className={css.profile}>hello</div>
+}
+
+// Profile.propTypes = {
+//   sections: PropTypes.object.isRequired
+// }
